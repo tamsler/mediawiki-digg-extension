ec3b2d3742f128011e4589b489fc37fc2f4fb50d
Thomas Amsler <tamsler@gmail.com>
2007-02-06 20:10:08 +0000

    Adjust for new syntax as published by digg.com at : http://digg.com/tools/integrate

--- digg.php
+++ digg.php
@@ -14,8 +14,39 @@ function wfDiggExtension() {
 
 # The callback function for converting the input text to HTML output
 function renderDigg( $input, $argv, &$parser ) {
-    $output = "<script>digg_url = '$input';</script><script src='http://digg.com/api/diggthis.js'></script>";
-    return $output;
+	
+	if(isset($argv["bgcolor"])) {	
+		$diggBgColor = trim($argv["bgcolor"]);
+	} 
+	else {
+		$diggBgColor = "";
+	}
+	if(isset($argv["style"])) {
+		$diggStyle = trim($argv["style"]);
+	}
+	else {
+		 $diggStyle = "";
+	}
+	
+	if($diggStyle !== "compact") {
+		$diggStyle = "";
+	}
+
+
+	if(empty($diggBgColor) && empty($diggStyle)) { 
+		$output = "<script type='text/javascript'>digg_url = '$input';</script><script src='http://digg.com/api/diggthis.js' type='text/javascript'></script>";
+	}
+	elseif(empty($diggBgColor) && !empty($diggStyle)) {
+		$output = "<script type='text/javascript'>digg_url = '$input'; digg_skin = '$diggStyle'; digg_bgcolor = '';</script><script src='http://digg.com/api/diggthis.js' type='text/javascript'></script>";
+	}
+	elseif(!empty($diggBgColor) && empty($diggStyle)) {
+		$output = "<script type='text/javascript'>digg_url = '$input'; digg_skin = ''; digg_bgcolor = '$diggBgColor';</script><script src='http://digg.com/api/diggthis.js' type='text/javascript'></script>";
+	}
+	else {
+		$output = "<script type='text/javascript'>digg_url = '$input'; digg_skin = '$diggStyle'; digg_bgcolor = '$diggBgColor';</script><script src='http://digg.com/api/diggthis.js' type='text/javascript'></script>";
+	}    
+	
+	return $output;
 }
 ?>
 
