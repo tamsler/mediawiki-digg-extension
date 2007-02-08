d7d6339fcfa1c7b268bd08fc04e943b373c9c61d
Thomas Amsler <tamsler@gmail.com>
2007-02-08 05:12:52 +0000

    Adding lgpl license, version, and author information.

--- digg.php
+++ digg.php
@@ -1,9 +1,45 @@
 <?php
+ 
+ /**
+ * mediawiki-digg-extension : "Show Digg Counts on Your Site".
+ * Copyright (C) 2007 Thomas Amsler
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ **/
+ 
 
-##
-# Digg.com "Show Digg Counts on Your Site" Code:
-# http://digg.com/tools/count
-##
+ /**
+ *
+ * @version 1.0.1
+ * @author Thomas Amsler
+ * @copyright Thomas Amsler 2007
+ *
+ **/
+
+$wdVersion = '1.0.1';
+
+$wgExtensionCredits['other'][] = array
+  (
+   'name' => "Show Digg.com Counts On Your Site",
+   'version' => $wdVersion,
+   'author' => 'Thomas Amsler',
+   'url' => 'http://thomasamsler.org/wiki/MediaWiki',
+   'description' => 'Show Digg.com Counts On Your Site'
+   );
+
 
 $wgExtensionFunctions[] = "wfDiggExtension";
 
